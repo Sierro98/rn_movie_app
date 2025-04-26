296413fb78663f99ec388d178bd724ca75cd58df
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2025-04-26 20:57:11 +0200

    created tab views as well as added assets

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,9 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
+    <View className="flex-1 justify-center items-center gap-10">
       <Text className="text-2xl text-primary font-bold">Welcome to my first RN App!</Text>
     </View>
   );
