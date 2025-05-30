99e5fb055eeb4e4a26679fe03211a1ad76f7b291
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2025-05-30 10:09:42 +0200

    added the call to the api in the home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,22 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { router } from "expo-router";
-import { Text, View, Image, ScrollView } from "react-native";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { router, useRouter } from "expo-router";
+import { Text, View, Image, ScrollView, ActivityIndicator } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+  // Fetching movies with an empty query to get all movies
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({
+    query: ''
+  }));
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -12,15 +24,30 @@ export default function Index() {
         className="flex-1 px-5"
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{
-          minHeight:"100%",
-          paddingBottom:10
-          }}>
+          minHeight: "100%",
+          paddingBottom: 10
+        }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => router.push("/search")}
-            placeholder='Search for a movie'/>
-        </View>
+        {moviesLoading ? (
+          <ActivityIndicator
+            size='large'
+            color='#0000ff'
+            className="mt-10 self-center" />
+        ) : moviesError ? (
+          <Text className="text-white">Error: {moviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder='Search for a movie' />
+            <>
+              <Text
+                className="text-white text-lg font-bold mt-5 mb-3">
+                Latest Movies
+              </Text>
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
