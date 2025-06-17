67a1ea25ca46496fec2d9d292a3b0144ae57b419
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2025-06-17 20:16:33 +0200

    pull to refresh added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,18 +5,34 @@ import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
 import useFetch from "@/services/useFetch";
 import { router, useRouter } from "expo-router";
-import { Text, View, Image, ActivityIndicator, FlatList } from "react-native";
+import { Text, View, Image, ActivityIndicator, FlatList, RefreshControl } from "react-native";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import * as Haptics from 'expo-haptics';
+
+import React, { useState } from 'react';
 
 export default function Index() {
   const router = useRouter();
+  const { top } = useSafeAreaInsets();
+  const [refreshing, setRefreshing] = useState(false);
+
   // Fetching movies with an empty query to get all movies
   const {
     data: movies,
     loading: moviesLoading,
     error: moviesError,
+    refetch: reFetchMovies,
   } = useFetch(() => fetchMovies({
     query: ''
   }));
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    await reFetchMovies();
+    setRefreshing(false);
+  };
+
 
   if (moviesLoading) {
     return (
@@ -56,11 +72,6 @@ export default function Index() {
         ListHeaderComponent={
           <>
             <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-            <SearchBar
-              onPress={() => router.push("/search")}
-              placeholder='Search for a movie' value={""} onChangeText={function (text: string): void {
-                throw new Error("Function not implemented.");
-              } } />
             <Text className="text-white text-lg font-bold mt-5 mb-5">
               Latest Movies
             </Text>
@@ -72,6 +83,14 @@ export default function Index() {
           paddingTop: 0,
         }}
         showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl 
+            refreshing={refreshing} 
+            onRefresh={onRefresh} 
+            tintColor="#fff" 
+            progressViewOffset={top + 10} 
+          />
+        }
       />
     </View>
   );
